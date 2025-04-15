89c15160e7ec2156192f81f4b52559cbe7cfec8d
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-04-15 21:14:54 +0530

    Footer completed

---
+++ src/components/Footer/FooterCopyright.jsx
@@ -0,0 +1,16 @@
+function FooterCopyright() {
+  return (
+    <p className="text-center text-lg mt-4">
+      © 2025{' '}
+      <a
+        href="#"
+        className="text-2xl hover:bg-gradient-to-r from-blue-500 to-pink-500 hover:text-transparent bg-clip-text"
+      >
+        sharifiqbaljamil.dev
+      </a>{' '}
+      | All rights reserved
+    </p>
+  );
+}
+
+export default FooterCopyright;
