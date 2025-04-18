1705be7e6e6ea7a2db57e1d63071a5872901735a
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-04-18 21:38:28 +0530

    Project reboot

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,7 @@
-import { useRef } from 'react';
-import MouseFollower from './components/Global/MouseFollower';
-import SplashCursor from './components/Global/SplashCursor';
-import Header from './components/Header/Header';
-import Loader from './components/Loader/Loader';
-import { useGSAP } from '@gsap/react';
-import gsap from 'gsap';
-import About from './components/About/About';
-import CurrentLearningAndFutureGoals from './components/Current Learning & Future Goals/CurrentLearningAndFutureGoals';
-import SkillsAndTools from './components/Skills & Tools/SkillsAndTools';
-import Experience from './components/Experience/Experience';
-import Card from './Card';
-import Footer from './components/Footer/Footer';
 function App() {
-  const main = useRef(null);
-
-  useGSAP(() => {
-    gsap.from(main.current, {
-      opacity: 0,
-      // duration: 1.8,
-      // delay: 5.2,
-      onComplete: () => {},
-    });
-  });
-
   return (
-    <div className="bg-zinc-950 text-white">
-      {/* <SplashCursor />
-      <MouseFollower />
-      <Loader /> */}
-      <main ref={main}>
-        <Header />
-        <About />
-        <CurrentLearningAndFutureGoals />
-        <SkillsAndTools />
-        <Experience />
-        <Card />
-        <Footer />
-      </main>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
